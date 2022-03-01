b11b5ae7283ddc1f37c252f5ec430eae7880f671
Connor Keenum <Nellakprime@gmail.com>
2022-02-28 19:09:48 -0600

    Made entry point for my single page application

---
+++ timer-project-react/src/App.js
@@ -0,0 +1,12 @@
+// I put all my components here, because this is a single page application
+import Timer from "./Components/Timer"
+import "./App.css"; // basic styles
+function App() {
+  return (
+    <>
+      <Timer/>
+    </>
+  );
+}
+
+export default App;
